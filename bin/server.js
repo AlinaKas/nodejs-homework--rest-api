2c9e867aaff44d1b3c0ae4fffbe0857e246940b6
Alina Kasianova <avkasyanova@gmail.com>
2022-01-15 16:23:47 +0200

    avatar logic fix

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,6 @@
 const mongo = require("mongoose");
 const app = require("../app");
-const { mkdir } = require("fs/promises");
+// const { mkdir } = require("fs/promises");
 const { colors } = require("colors");
 
 const { PORT = 5000, DB_HOST } = process.env;
@@ -9,7 +9,7 @@ mongo
   .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      // mkdir(process.env.UPLOAD_DIR, { recursive: true }); //создаем папку для временной загрузки аватарок, если ее нет
+      // mkdir(process.env.UPLOAD_DIR, { recursive: true });
       console.log(`Server running. Use our API on port: ${PORT}`.cyan);
       console.log("Database connection successful".cyan);
     });
